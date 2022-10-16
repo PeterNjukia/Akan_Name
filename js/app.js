95c7a92de98fc6769d663aee2916db634fed66a4
chunuz <pnjukia@gmail.com>
2022-10-16 14:44:45 +0300

    app.js/index updates

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 
 
+function getUserDetails(){
+
+    var Year= prompt("Enter year of birth")
+    var Month=prompt("Enter Month")
+    var day=prompt("Enter day of the month")
+    let Gender=prompt("Enter Male or Female")
+}
+
 
 
 const MaleAkai = {Sunday: "Kwasi", Monday: "Kwadwo",Tuesday: "Kwabena",Wednesday: "Kwaku",Thursday:  "Yaw",Friday: "Kofi",Saturday: "Kwame"};
 const Femalekai = {Sunday: "Akosua",Monday: "Adwoa",Tuesday: "Abenaa",Wednesday: "Akua",Thursday:  "Yaa",Friday: "Afua",Saturday: "Ama"};
 
-console.log(MaleAkai.Monday)
-console.log(Femalekai.Monday)
+const AkaiName = Object.values(MaleAkai)[6];
+console.log(AkaiName);
+
+for (i in MaleAkai) {
+    if (i==function return) {
+        alert(MaleAkai[keyindex])
+    }else{
+        alert(Femalekai[keyindex])
+    }
+
+}
