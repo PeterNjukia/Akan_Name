f92b69e84ad0bac57ad165530efa46a940cdf869
chunuz <pnjukia@gmail.com>
2022-10-16 14:43:27 +0300

    app.js/index updates

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+
+
+
+const MaleAkai = {Sunday: "Kwasi", Monday: "Kwadwo",Tuesday: "Kwabena",Wednesday: "Kwaku",Thursday:  "Yaw",Friday: "Kofi",Saturday: "Kwame"};
+const Femalekai = {Sunday: "Akosua",Monday: "Adwoa",Tuesday: "Abenaa",Wednesday: "Akua",Thursday:  "Yaa",Friday: "Afua",Saturday: "Ama"};
+
+console.log(MaleAkai.Monday)
+console.log(Femalekai.Monday)
