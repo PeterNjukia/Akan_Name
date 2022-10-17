81cfbc7c802c5ee2f0299e44a2537da2456ee2c6
chunuz <pnjukia@gmail.com>
2022-10-17 12:57:20 +0300

    js updates

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 
-const MALE_NAMES = ["Kwasi", 10, 20, 30, 40, 50, 60];
-const FEMALE_NAMES = ["Akosua", 70, 80, 90, 100, 110];
-const DAYS_OF_THE_WEEK = ["Sunday"];
+function dayofbirth(){
 
-function getUserInputAndCalculateDay(){
     var year  = prompt("Enter year: ");
     var century = parseInt(year.substring(0, 2));
     year = parseInt(year);
     var month = parseInt(prompt("Enter month: "));
     var day = parseInt(prompt("Enter day: "));
     var dayOfTheWeek = Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
-    return dayOfTheWeek; // 0 - 6
+    return dayOfTheWeek; 
 }
 
 function getGender(){
@@ -18,18 +15,23 @@ function getGender(){
     return gender;
 }
 
-function main(){
-    var day = getUserInputAndCalculateDay();
-    var gender = getGender();
-    var akanName = null;
 
-    if (gender.toLowerCase().startsWith("f")){
-        akanName = FEMALE_NAMES[day];
-    } else {
-        akanName = MALE_NAMES[day];
-    }
-    alert("Your Akan name is " + akanName);
-}
+function main(){
+    var day = dayofbirth();
+    var gender = getGender();
+    // var akanName = null;
+    const MaleAkai = {Sunday: "Kwasi", Monday: "Kwadwo",Tuesday: "Kwabena",Wednesday: "Kwaku",Thursday:  "Yaw",Friday: "Kofi",Saturday: "Kwame"};
+    const Femalekai = {Sunday: "Akosua",Monday: "Adwoa",Tuesday: "Abenaa",Wednesday: "Akua",Thursday:  "Yaa",Friday: "Afua",Saturday: "Ama"};
+    const Boy = Object.values(MaleAkai)[day];
+    const Girl = Object.values(Femalekai)[day];
+
+    if (gender=="male"){
+            alert(Boy);
+    }else {
+            alert(Girl);
+    };
+    
+};
 
 main()
 
