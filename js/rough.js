0bd0b4e8addcbeeb734bfffec9cccf4aa638d84d
chunuz <pnjukia@gmail.com>
2022-10-17 15:33:51 +0300

    js updates

--- js/rough.js
+++ js/rough.js
@@ -3,8 +3,9 @@
 function GetAkaiDayFromUserInputs(){
 
     var year  = prompt("Enter year: ");
-    var century = parseInt(year.substring(0, 2));
+    var century = parseInt(year.substring(0,2));
     year = parseInt(year);
+    var yearcode=year.substring(2,4);
     var month = parseInt(prompt("Enter month: "));
     var day = parseInt(prompt("Enter day: "));
     var dayOfTheWeek = Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
